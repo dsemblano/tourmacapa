7eb0b6958e03c08611f0d39a800ab38233b474a1
Daniel Semblano <dsemblano@gmail.com>
2025-03-26 17:22:07 -0300

    testing fix theme css with blue

--- site/web/app/themes/tourmacapa/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/tourmacapa/resources/views/partials/home/hero.blade.php
@@ -1,4 +1,4 @@
-<section id="hero" class="bg-red-600">
+<section id="hero" class="bg-blue-600">
     {{-- {!! do_shortcode('[voucher_validation]') !!} --}}
     <div class="container">
         <div class="flex flex-col lg:flex-row">
