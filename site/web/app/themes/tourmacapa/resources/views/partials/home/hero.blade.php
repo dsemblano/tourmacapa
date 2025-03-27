5dc3d0c89fbe158aadb3c88cdc939e58a8f33612
Daniel Semblano <dsemblano@gmail.com>
2025-03-27 14:11:00 -0300

    Home: layout improvements

--- site/web/app/themes/tourmacapa/resources/views/partials/home/hero.blade.php
+++ site/web/app/themes/tourmacapa/resources/views/partials/home/hero.blade.php
@@ -6,10 +6,13 @@
                 <img id="logoname" class="hover:scale-110 transition duration-300 ease-in-out" width="200" height="280"
                 src="{{ asset('resources/images/TucuFoodlogo.png') }}" alt="Tour Macapá página inicial" />
 
-                <span class="text-4xl inline-block text-white bg-primaryColor font-bold p-2">Compre 1, Leve 2</span>
+                <span class="text-4xl inline-block bg-primaryColor font-bold p-2">Compre 1, Leve 2</span>
                 <h1 class="text-8xl hero-heading font-bold text-grayH">
                     Tour<br /> Macapá
                 </h1>
+                <p class="mt-7 text-3xl text-grayH">
+                    Os melhores estabelecimentos da cidade e região!
+                </p>
                 <p class="mt-7 text-3xl text-grayH">
                     Vouchers e promoções para os melhores estabelecimentos do Amapá!
                 </p>
