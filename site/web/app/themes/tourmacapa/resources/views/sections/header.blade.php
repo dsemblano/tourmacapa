e74af16ce32b4f8bd9192f9a8f2a98526edf6f62
Daniel Semblano <dsemblano@gmail.com>
2025-03-30 00:12:53 -0300

    Header with logo css improvements

--- site/web/app/themes/tourmacapa/resources/views/sections/header.blade.php
+++ site/web/app/themes/tourmacapa/resources/views/sections/header.blade.php
@@ -1,6 +1,6 @@
 <header class="banner border-gray-100 border-b-2 ">
   <div class="container">
-    <div class="flex flex-wrap lg:flex-nowrap py-4">
+    <div class="flex flex-row justify-between lg:justify-start lg:flex-wrap">
       <div class="logo">
           @include('partials.logo')
         {{-- <a class="brand" href="{{ home_url('/') }}">
@@ -9,7 +9,7 @@
         </a> --}}
       </div>
       @if (has_nav_menu('primary_navigation'))
-      <nav class="nav-primary container py-2 nav-primary">
+      <nav class="nav-primary">
         <div class="flex flex-wrap lg:flex-nowrap justify-between items-center mx-auto">   
           @include('partials.menu')
         </div>
