024367ab9387bd6a917e401bcc8e8b223c633bbf
Daniel Semblano <dsemblano@gmail.com>
2025-04-01 00:48:13 -0300

    ACF: template cadastrolayout

--- site/web/app/themes/tourmacapa/resources/views/cadastrolojas.blade.php
+++ site/web/app/themes/tourmacapa/resources/views/cadastrolojas.blade.php
@@ -7,18 +7,22 @@
 
 @section('content')
 
-<?php 
-acf_form(array(
-  'post_id'       => 'new_post', // Save as new post
-  'post_title'    => false, // Hide title field
-  'post_content'  => false, // Hide content field
-  'fields'        => array('nome_estabelecimento', 'email', 'cnpj', 'telefone', ), // Your ACF field keys
-  'submit_value'  => 'Enviar cadastro',
-  'new_post'      => array(
-    'post_type'   => 'cadastro-loja', // Custom post type
-    'post_status' => 'publish',
-  ),
-));
-?>
+<section id="cadastro_lojas" class="bg-slate-100 lg:arrow-border py-6">
+    <div class="container">
+        <?php 
+        acf_form(array(
+        'post_id'       => 'new_post', // Save as new post
+        'post_title'    => false, // Hide title field
+        'post_content'  => false, // Hide content field
+        'fields'        => array('nome_estabelecimento', 'email', 'cnpj', 'telefone', ), // Your ACF field keys
+        'submit_value'  => 'Enviar cadastro',
+        'new_post'      => array(
+            'post_type'   => 'cadastro-loja', // Custom post type
+            'post_status' => 'publish',
+        ),
+        ));
+        ?>
+    </div>
+</section>
 
 @endsection
