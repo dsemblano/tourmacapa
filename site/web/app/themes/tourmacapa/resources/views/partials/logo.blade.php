1c48d458a4f9a15c448ca4ddc9bb9c9b96e6c349
Daniel Semblano <dsemblano@gmail.com>
2025-04-01 13:41:52 -0300

    Logo header with logo name text

--- site/web/app/themes/tourmacapa/resources/views/partials/logo.blade.php
+++ site/web/app/themes/tourmacapa/resources/views/partials/logo.blade.php
@@ -1,3 +1,13 @@
 <a class="brand relative" href="{{ home_url('/') }}">
-@include('partials/icons.logoTour')
+
+<div id="logo" class="flex flex-row items-center">
+
+    @include('partials/icons.logoTour')
+    <div id="logo_tour" class="flex flex-col relative top-1">
+        <span class="text-base relative">Tour</span>
+        <span class="text-base relative bottom-1.5">Macapá</span>
+    </div>
+
+</div>
+
 </a>
